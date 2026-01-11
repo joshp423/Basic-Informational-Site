2477ed93496b4b8d84af94700cc223e511ea90bb
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-01-11 16:28:10 +1300

    finished routing

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const http = require('node:http');
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
 
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = '';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        case '/contact':
+            path += 'contact-me.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path,(err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 server.listen(8080, 'localhost', () => {
